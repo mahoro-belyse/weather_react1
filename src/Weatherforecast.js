bcf98b0079bb371ac655dccdf3b437dcb174a720
mahoro-belyse <151196672+mahoro-belyse@users.noreply.github.com>
2024-11-28 12:15:20 +0200

    done working in weather react

--- src/Weatherforecast.js
+++ src/Weatherforecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weatherforecast.css";
 import axios from "axios";
 import Weatherforecastday from "./Weatherforecastday";
@@ -6,6 +6,9 @@ import Weatherforecastday from "./Weatherforecastday";
 export default function Weatherforecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,9 +19,15 @@ export default function Weatherforecast(props) {
     return (
       <div className="weatherforecast">
         <div className="row">
-          <div className="col">
-            <Weatherforecastday data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyforecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <Weatherforecastday data={dailyforecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
@@ -26,7 +35,7 @@ export default function Weatherforecast(props) {
     let apiKey = "eac360db5fc86ft86450f3693e73o43f";
     let longitude = props.coordinates.longitude;
     let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
